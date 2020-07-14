c0a648c0fab63dd61cd5dab293f736bac8958d67
Nathaliaaguiar <nathaliaaguiar444@gmail.com>
2020-07-14 14:07:51 -0300

    2 files modified, 6 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,13 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
+import { PageNavComponent } from './page-nav/page-nav.component';
+import { PageFooterComponent } from './page-footer/page-footer.component';
+import { PageHeaderComponent } from './page-header/page-header.component';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
+  declarations: [ AppComponent, HelloComponent, PageNavComponent, PageFooterComponent, PageHeaderComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
