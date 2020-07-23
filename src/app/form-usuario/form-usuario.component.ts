10f3caf390f3d9f6b70f05e6662ad9794a8c94a5
Nathaliaaguiar <nathaliaaguiar444@gmail.com>
2020-07-23 13:20:57 -0300

    usuario

--- src/app/form-usuario/form-usuario.component.ts
+++ src/app/form-usuario/form-usuario.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { Usuario } from '../..models/usuario'
+import { Usuario } from '../.models/usuario'
 
 @Component({
   selector: 'app-form-usuario',
